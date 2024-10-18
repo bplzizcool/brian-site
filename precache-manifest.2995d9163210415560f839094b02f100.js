d61461c714349baea440764ba8ab4dd5a92164dc
Brian Lane <bl185105@mac-c02g3648md6.attlocal.net>
2024-10-18 11:30:52 -0400

    Updates

---
+++ precache-manifest.2995d9163210415560f839094b02f100.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c7d299042983d8e1b96",
+    "url": "/brian-site/static/css/main.9fe85b12.chunk.css"
+  },
+  {
+    "revision": "0c7d299042983d8e1b96",
+    "url": "/brian-site/static/js/main.891adda9.chunk.js"
+  },
+  {
+    "revision": "23adf913a177d87a351c",
+    "url": "/brian-site/static/js/runtime~main.147d14e5.js"
+  },
+  {
+    "revision": "9c777f497d47f11021f9",
+    "url": "/brian-site/static/css/2.03cbce4e.chunk.css"
+  },
+  {
+    "revision": "9c777f497d47f11021f9",
+    "url": "/brian-site/static/js/2.d08898cd.chunk.js"
+  },
+  {
+    "revision": "e1d1374046e714c5048d962f6dc80e42",
+    "url": "/brian-site/static/media/profile_pic.e1d13740.jpg"
+  },
+  {
+    "revision": "67edcff8f089d9f2040bae3eb463d29e",
+    "url": "/brian-site/static/media/demo.67edcff8.svg"
+  },
+  {
+    "revision": "921ade6df0c98c8bd0181e9b45962687",
+    "url": "/brian-site/index.html"
+  }
+];
