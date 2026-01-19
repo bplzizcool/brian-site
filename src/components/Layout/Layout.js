502528e0cebd4950ea9efebcae9238e6bed68b91
Brian Lane <bplane92@gmail.com>
2026-01-19 13:19:42 -0500

    Creating new component for partical background

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Container, Box } from '@mui/material';
 import { styled } from '@mui/material/styles';
+import Particles from '../GalaxyBackground/Particles';
 
 const StyledBox = styled(Box)(({ theme }) => ({
     minHeight: '100vh',
     width: '100%',
-    background: 'linear-gradient(135deg, #232526 0%, #414345 100%)',
+    background: 'linear-gradient(45deg, #000000, #414345 100%)',
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
@@ -13,6 +14,17 @@ const StyledBox = styled(Box)(({ theme }) => ({
     paddingTop: theme.spacing(8),
     overflow: 'hidden',
 }));
+
+const StyledParticlesBackground = styled(Box)(({ theme }) => ({
+    width: '100vw',
+    height: '100vh',
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    zIndex: 0,
+    pointerEvents: 'none',
+}));
+
 const StyledContainer = styled(Container)(({ theme }) => ({
     background: 'rgba(255,255,255,0.04)',
     borderRadius: 16,
@@ -25,11 +37,31 @@ const StyledContainer = styled(Container)(({ theme }) => ({
     },
 }));
 
+const StyledDivWrapper = styled('div')`
+    position: relative;
+    z-index: 1;
+`;
+
 const Layout = ({ children }) => (
     <StyledBox>
-        <StyledContainer maxWidth="md">
-            {children}
-        </StyledContainer>
+        <StyledParticlesBackground>
+            <Particles
+                particleColors={["#ffffff"]}
+                particleCount={200}
+                particleSpread={10}
+                speed={0.1}
+                particleBaseSize={100}
+                moveParticlesOnHover
+                alphaParticles={false}
+                disableRotation={false}
+                pixelRatio={1}
+            />
+        </StyledParticlesBackground>
+        <StyledDivWrapper>
+            <StyledContainer maxWidth="md">
+                {children}
+            </StyledContainer>
+        </StyledDivWrapper>
     </StyledBox>
 );
 
